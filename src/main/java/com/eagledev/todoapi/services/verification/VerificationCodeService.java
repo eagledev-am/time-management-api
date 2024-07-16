a4774979db25ce90d9fd0c9a21e351d1d5795ddc
eagledev-am <magdyabdo@gmail.com>
2024-07-16 16:44:10 +0300

    feat: implement verification code service

---
+++ src/main/java/com/eagledev/todoapi/services/verification/VerificationCodeService.java
@@ -0,0 +1,8 @@
+package com.eagledev.todoapi.services.verification;
+
+import com.eagledev.todoapi.entities.User;
+
+public interface VerificationCodeService {
+    String generateVerificationCode(User user);
+    Boolean validateVerificationCode(String verificationCode , String userName);
+}
---
+++ src/main/java/com/eagledev/todoapi/services/verification/VerificationCodeServiceImp.java
@@ -0,0 +1,44 @@
+package com.eagledev.todoapi.services.verification;
+
+import com.eagledev.todoapi.entities.User;
+import com.eagledev.todoapi.entities.VerificationCode;
+import com.eagledev.todoapi.exceptions.VerificationException;
+import com.eagledev.todoapi.repos.VerificationCodeRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class VerificationCodeServiceImp implements VerificationCodeService {
+    private final VerificationCodeRepo repo;
+
+    @Override
+    public String generateVerificationCode(User user) {
+        String verificationCode = generateVerificationCode();
+        VerificationCode code = VerificationCode.builder()
+                        .code(verificationCode)
+                        .user(user)
+                        .expiryDate(Instant.now().plusSeconds(15 * 60))
+                        .build();
+        repo.save(code);
+        return verificationCode;
+    }
+
+    @Override
+    public Boolean validateVerificationCode(String code , String userName) {
+        VerificationCode verificationCode = repo.findVerificationCodeByCode(code)
+                .orElseThrow(() -> new VerificationException("Invalid verification code !"));
+        User userToVerify = verificationCode.getUser();
+        return userName.equals(userToVerify.getUsername())
+                && verificationCode.getExpiryDate().isAfter(Instant.now());
+    }
+
+    private String generateVerificationCode(){
+        Random random = new Random();
+        int code = 100000 + random.nextInt(900000); // generate a random 6-digit number
+        return String.valueOf(code);
+    }
+}
