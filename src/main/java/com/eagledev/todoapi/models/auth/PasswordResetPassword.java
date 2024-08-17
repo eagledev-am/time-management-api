cbbbc5e85a2a0ba785457e33fd923e33c5b2d825
eagledev-am <magdyabdo@gmail.com>
2024-08-17 04:25:39 +0300

    refactor: move required models for auth to auth package

---
+++ src/main/java/com/eagledev/todoapi/models/auth/PasswordResetPassword.java
@@ -0,0 +1,23 @@
+package com.eagledev.todoapi.models.auth;
+
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class PasswordResetPassword {
+
+    @NotNull(message = "This field is required !")
+    String emailOrUserName;
+
+    @NotNull(message = "Password field is required !")
+    @Size(min = 6, max = 20, message = "Password length must be between 6 and 20 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
+    String newPassword;
+
+    String code;
+}
