82ee32dcdb930eae99f4338b2ae9949755aa774e
eagledev-am <magdyabdo@gmail.com>
2024-08-19 15:26:29 +0300

    chore: add user model

---
+++ src/main/java/com/eagledev/todoapi/models/user/UserModel.java
@@ -0,0 +1,27 @@
+package com.eagledev.todoapi.models.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.UUID;
+
+@Setter
+@Getter
+public class UserModel {
+    @JsonProperty("id")
+    private UUID uuid ;
+
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("firstname")
+    private String firstName ;
+
+    @JsonProperty("lastname")
+    private String lastName ;
+
+    @JsonProperty("photo_url")
+    private String profilePictureUrl ;
+
+    private String email;
+}
