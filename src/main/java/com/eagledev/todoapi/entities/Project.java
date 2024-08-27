cf04ae97ea8fa511de675abc49980a30aad20f40
eagledev-am <magdyabdo@gmail.com>
2024-08-27 16:09:14 +0300

    feat: Add Project entity

---
+++ src/main/java/com/eagledev/todoapi/entities/Project.java
@@ -0,0 +1,41 @@
+package com.eagledev.todoapi.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+@ToString
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private String avaterUrl;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "project" , cascade = CascadeType.ALL)
+    private Set<Task> taskSet;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL )
+    private Set<ProjectTeam> project_members;
+}
