5c3aa34a9733ccb7c8d278a58526995cf17a7ebc
eagledev-am <magdyabdo@gmail.com>
2024-08-27 16:08:40 +0300

    feat: Add Notification entity

---
+++ src/main/java/com/eagledev/todoapi/entities/Notification.java
@@ -0,0 +1,35 @@
+package com.eagledev.todoapi.entities;
+
+import com.eagledev.todoapi.entities.enums.NotificationStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private String message;
+
+    private LocalDateTime creationTime;
+
+    @Enumerated(EnumType.STRING)
+    NotificationStatus notificationStatus;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+}
