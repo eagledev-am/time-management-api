628fa352f49684dc76128b785db2ce43197739c0
eagledev-am <magdyabdo@gmail.com>
2024-09-11 17:44:51 +0300

    chore: Add ResourceNotFound Exception

---
+++ src/main/java/com/eagledev/timemanagement/exceptions/ResourceNotFound.java
@@ -0,0 +1,7 @@
+package com.eagledev.timemanagement.exceptions;
+
+public class ResourceNotFound extends RuntimeException {
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+}
