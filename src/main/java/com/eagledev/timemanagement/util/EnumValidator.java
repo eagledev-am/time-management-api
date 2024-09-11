55257936838f309859e231d73df3e69db1a17857
eagledev-am <magdyabdo@gmail.com>
2024-09-11 17:44:12 +0300

    chore: Add EnumValidator annotation impl.

---
+++ src/main/java/com/eagledev/timemanagement/util/EnumValidator.java
@@ -0,0 +1,24 @@
+package com.eagledev.timemanagement.util;
+
+import com.eagledev.timemanagement.entities.enums.TaskPriority;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.Arrays;
+
+public class EnumValidator implements ConstraintValidator<ValidEnum, TaskPriority> {
+    private Class<? extends Enum<?>> enumClass;
+
+    @Override
+    public void initialize(ValidEnum constraintAnnotation) {
+        this.enumClass = constraintAnnotation.enumClass();
+    }
+
+    @Override
+    public boolean isValid(TaskPriority value, ConstraintValidatorContext context) {
+        if(value == null)
+            return true;
+        return Arrays.stream(enumClass.getEnumConstants())
+                .anyMatch(e -> e.name().equals(value.name()));
+    }
+}
---
+++ src/main/java/com/eagledev/timemanagement/util/ValidEnum.java
@@ -0,0 +1,16 @@
+package com.eagledev.timemanagement.util;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Constraint(validatedBy = EnumValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEnum {
+    String message() default "Invalid enum value";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<? extends Enum<?>> enumClass();
+}
